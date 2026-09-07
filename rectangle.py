ac67f84cf828da1e81dcd3d2b3dd4a30e1d0cf01
Jacara04 <jcoates8@fingerlakes.edu>
2026-09-07 16:07:05 +0000

    shapes

---
+++ rectangle.py
@@ -0,0 +1,61 @@
+# FILE NAME: rectangle.py
+
+# NAME: Jacara coates   
+# DATE: 09/07/2026
+# BRIEF DESCRIPTION:  Creating a script that displays a rectangle
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+print("*****")
+print("*   *")
+print("*   *")
+print("*****") 
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+*****
+*   *
+*   *
+*****
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. This is your first "real" lab. What is one thing that you struggled with?
+
+
+
+
+
+'''
