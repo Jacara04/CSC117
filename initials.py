273d0e536b766893cddc8fa05da3a096a8d4130a
Jacara04 <jcoates8@fingerlakes.edu>
2026-09-08 01:01:29 +0000

    initials

---
+++ initials.py
@@ -0,0 +1,66 @@
+# FILE NAME: initials.py
+
+# NAME: Jacara Coates
+# DATE: 09/07/2026
+# BRIEF DESCRIPTION:  A block letter representation of my initials.
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+print("     JJJJ        CCCCCCCCC")
+print("     JJJJ        CCCCCCCCC")
+print("     JJJJ        CCCC")
+print("     JJJJ        CCCC")
+print("     JJJJ        CCCC")
+print("JJJJJJJJJ        CCCCCCCCC")
+print("JJJJJJJJJ        CCCCCCCCC")
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+TTTTTTTT     JJJJJJJJ
+   TT            JJ
+   TT            JJ
+   TT        JJ  JJ
+   TT         JJ JJ
+   TT           JJ
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. Did you have to submit this code more than once before it passed? If so, what
+   did you have to change?
+
+
+
+
+
+'''
